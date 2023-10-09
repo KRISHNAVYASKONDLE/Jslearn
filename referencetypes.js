90fbe2ee6b57633fa5dd9b487d43635b6f3bc3ff
KRISHNAVYASKONDLE <krishnavyaskondle@gmail.com>
2023-10-09 23:43:18 +0530

    done with datatypes and letvar intro

---
+++ referencetypes.js
@@ -0,0 +1,26 @@
+
+let id=Symbol('123')
+console.log(id)
+
+let id2=Symbol('123')
+console.log(id==id2)
+
+//object
+myobj={
+    num:1,
+    name:"sdf"
+}
+console.log(myobj)
+
+//array
+myArr=[1,2,"dd"]
+console.log(myArr)
+
+//fun
+myfun=function(id){
+    console.log("llu"+" "+id)
+}
+
+myfun(3);
+myfun("er3");
+
